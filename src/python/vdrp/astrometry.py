aad31a19f6886634803cca603370e88ed23a3a5e
Maximilian Fabricius <mxhf@mpe.mpg.de>
2018-07-24 16:13:37 +0200

    Fixed logging.

--- src/python/vdrp/astrometry.py
+++ src/python/vdrp/astrometry.py
@@ -808,15 +808,22 @@ def main():
     # command line parameters overwrite config file.
     args = parseArgs()
 
-    # Set up logging to file - see previous section for more details
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+    # set up logging to file - see previous section for more details
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)-8s %(message)s',
                         datefmt='%m-%d %H:%M',
-                        handlers=[
-                            logging.FileHandler(args.logfile),
-                            logging.StreamHandler()
-                        ],
+                        filename=args.logfile,
                         filemode='w')
+    # define a Handler which writes INFO messages or higher to the sys.stderr
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    # set a format which is simpler for console use
+    formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+    # tell the handler to use this format
+    console.setFormatter(formatter)
+    # add the handler to the root logger
+    logging.getLogger('').addHandler(console)
+
 
     # Create work directory for given night and shot
     cwd = os.getcwd()
