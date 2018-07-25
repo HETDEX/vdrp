1d601c2ca3555d929cd7def137061ed50d2f70f4
Maximilian Fabricius <mxhf@mpe.mpg.de>
2018-07-25 14:57:16 +0200

    Repared parameters in runall1.sh. Switched to all mode.

--- runall1.sh
+++ runall1.sh
@@ -10,9 +10,9 @@ TRACK=1
 CFG=vdrp.config
 
 # Either run
-# python vdrp/src/python/vdrp/astrometry.py -c vdrp.config $NIGHT $TARGET $RA $TRACK 1
+vdrp/src/python/vdrp/astrometry.py -c $CFG $NIGHT $TARGET $RA $DEC $TRACK
 # or
-# python vdrp/src/python/vdrp/astrometry.py -c vdrp.config $NIGHT $TARGET $RA $TRACK $TRACK -t all
+# vdrp/src/python/vdrp/astrometry.py -c $CFG $NIGHT $TARGET $RA $DEC $TRACK -t all
 # to run all tasks.
 
 # Or run them one by one:
@@ -24,7 +24,7 @@ CFG=vdrp.config
 #vdrp/src/python/vdrp/astrometry.py -c $CFG $NIGHT $TARGET $RA $DEC $TRACK -t mktot
 #vdrp/src/python/vdrp/astrometry.py -c $CFG $NIGHT $TARGET $RA $DEC $TRACK -t rmaster
 #vdrp/src/python/vdrp/astrometry.py -c $CFG $NIGHT $TARGET $RA $DEC $TRACK -t flux_norm
-vdrp/src/python/vdrp/astrometry.py -c $CFG $NIGHT $TARGET $RA $DEC $TRACK -t redo_shuffle
+#vdrp/src/python/vdrp/astrometry.py -c $CFG $NIGHT $TARGET $RA $DEC $TRACK -t redo_shuffle
 #vdrp/src/python/vdrp/astrometry.py -c $CFG $NIGHT $TARGET $RA $DEC $TRACK -t get_ra_dec_orig
 #vdrp/src/python/vdrp/astrometry.py -c $CFG $NIGHT $TARGET $RA $DEC $TRACK -t compute_offset
 #vdrp/src/python/vdrp/astrometry.py -c $CFG $NIGHT $TARGET $RA $DEC $TRACK -t add_ifu_xy
@@ -33,4 +33,4 @@ vdrp/src/python/vdrp/astrometry.py -c $CFG $NIGHT $TARGET $RA $DEC $TRACK -t red
 
 # You can also group task together in a commaseparated list (without spaces)
 # this is slightly faster that executing astrometry.py multiple times.
-# python vdrp/src/python/vdrp/astrometry.py -c $CFG $NIGHT $TARGET $RA $DEC $TRACK -t cp_post_stamps,mk_post_stamp_matrix,daophot_find
+# vdrp/src/python/vdrp/astrometry.py -c $CFG $NIGHT $TARGET $RA $DEC $TRACK -t cp_post_stamps,mk_post_stamp_matrix,daophot_find
