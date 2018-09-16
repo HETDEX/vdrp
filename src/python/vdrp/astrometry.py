04a71032c31c1e5186bd6e8d988b59da9d41c8d7
Maximilian Fabricius <mxhf@mpe.mpg.de>
2018-09-16 06:02:53 -0500

    Adjusted fibcoords routine to use fplanes either from files or MPE database.

--- src/python/vdrp/astrometry.py
+++ src/python/vdrp/astrometry.py
@@ -57,7 +57,7 @@ from vdrp import cltools
 from vdrp import utils
 from vdrp.daophot import DAOPHOT_ALS
 from vdrp.utils import read_radec, write_radec
-from vdrp import fplane_client
+from vdrp.fplane_client import retrieve_fplane
 
 
 
@@ -549,56 +549,6 @@ def flux_norm(wdir, mag_max, infile='all.raw', outfile='norm.dat'):
         with open(outfile, 'w') as f:
             s = "{:10.8f} {:10.8f} {:10.8f}".format(n1, n2, n3)
             f.write(s)
-
-
-def retrieve_fplane(night, fplane_txt, wdir):
-    """ Saves the fplane file to the target directory
-    and names it fplane.txt.
-
-    Args:
-    fplane_txt (str) : Either a specific fplane file is specified here, 'DATABASE' is passed,
-                       or a file pattern is provided e.g. fplane_YYYYMMDD.txt.
-                       In case of the latter a substring of format YYYYMMDD is
-                       expected.  The routine will then search
-                       for an fplane file of the current date or pick the next
-                       older one. E.g. if shot 2080611v017 is to be analysed
-                       and fplane files fplane_2080610.txt and
-                       fplane_2080615.txt exist, then fplane_2080610.txt will
-                       be picked.  In the case of DATABASE the fplane file is
-                       retrieved from https://luna.mpe.mpg.de/fplane/.
-
-    """
-    global vdrp_info
-    target_filename =  os.path.join(wdir, "fplane.txt")
-    if fplane_txt == "DATABASE":
-        # fplane is retrieved from MPE server
-        logging.info("Retrieving fplane file from MPE server. ")
-        fplane_client.get_fplane(target_filename, datestr=night, actpos=False, full=True)
-    else:
-        if not "YYYYMMDD" in fplane_txt:
-            # a specific fplane is specified in the config file.
-            logging.info("Using {}.".format(fplane_txt))
-            shutil.copy2(fplane_txt, os.path.join(wdir, "fplane.txt"))
-            return
-        else:
-            # a fplane file pattern is specified in the config file.
-            # find all files that math the pattern
-            ff = glob.glob(fplane_txt.replace("YYYYMMDD", "????????"))
-            ff = np.array(ff)
-            # parse dates
-            dd = []
-            for f in ff:
-                m = re.match("(.*)(20\d{2}\d{2}\d{2})", f)
-                d = m.group(2)
-                dd.append(int(d))
-            dd = np.array(dd)
-            ii = np.argsort(dd)
-            dd = dd[ii]
-            ff = ff[ii]
-            jj = dd <= int(night)
-            source_filename = ff[jj][-1]
-            logging.info("Using {}.".format(source_filename))
-            shutil.copy2(source_filename, os.path.join(wdir, "fplane.txt"))
 
 
 
