2b7e70c60c3eabe053dea14013627ab1ce8df9ed
Maximilian Fabricius <mxhf@mpe.mpg.de>
2018-09-27 09:01:10 -0500

    Added fiber coordinate calculation to astrometry.py. Removed fibcoords.py and run_fibcoords.py, moved task definition to vdrp.config.

--- src/scripts/run_shifts.sh
+++ src/scripts/run_shifts.sh
@@ -10,6 +10,8 @@ TRACK=$5
 CFG=../${NIGHT}v${SHOT}.config
 LOG=${NIGHT}v${SHOT}.log
 
+echo $ADD_ARGS
+
 # Fall back to vdrp.config if no night/shot specific 
 # configuration file exists.
 if [ ! -f $CFG ]; then
@@ -17,31 +19,4 @@ if [ ! -f $CFG ]; then
 fi
 
 echo Configuration file $CFG
-# Either run
 ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK
-# or
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t all
-# to run all tasks.
-
-# Or run them one by one:
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t cp_post_stamps
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t mk_post_stamp_matrix
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t rename_cofes
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t daophot_find
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t daophot_phot_and_allstar
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t mktot
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t rmaster
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t flux_norm
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t get_ra_dec_orig
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG  -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t redo_shuffle
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t compute_offset
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t compute_with_optimal_ang_off
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t combine_radec
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t add_ifu_xy
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t mkmosaic
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t mk_match_plots
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t fibcoords
-
-# You can also group task together in a commaseparated list (without spaces)
-# this is slightly faster that executing astrometry.py multiple times.
-# ../vdrp/src/python/vdrp/astrometry.py --logfile $LOG -c $CFG $NIGHT $SHOT $RA $DEC $TRACK -t cp_post_stamps,mk_post_stamp_matrix,daophot_find
