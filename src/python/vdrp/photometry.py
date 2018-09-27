3fd080cebaad3e67c9b63df8dd43eb24711106f1
Jan Snigula <snigula@mpe.mpg.de>
2018-09-27 13:40:14 +0200

    Fixed reading shuffle stars some more

--- src/python/vdrp/photometry.py
+++ src/python/vdrp/photometry.py
@@ -578,7 +578,7 @@ def get_shuffle_stars(shuffledir, nightshot, maglim):
     try:
         indata = np.loadtxt(shuffledir + '/' + nightshot + '/shout.ifustars')
         for d in indata:
-            star = ShuffleStar( 20000 + c, d[1], d[2], d[3], d[4], d[5], d[6],
+            star = ShuffleStar(20000 + c, d[0], d[1], d[2], d[3], d[4], d[5], d[6],
                                d[7], d[8])
             if star.mag_g < maglim:
                 stars.append(star)
